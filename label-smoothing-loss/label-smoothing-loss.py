21ed9c9ab49392d426e5ecdecee9a4538483ca86
whoamipractice <priyadipsau10@gmail.com>
2026-09-12 18:25:09 +0530

    Solve label-smoothing-loss

--- label-smoothing-loss/label-smoothing-loss.py
+++ label-smoothing-loss/label-smoothing-loss.py
@@ -1,8 +1,8 @@
-import math
 import numpy as np
 
 def label_smoothing_loss(predictions: list, target: int, epsilon: float) -> float:
     """Returns cross-entropy loss for the smoothed target distribution."""
+    
     p = np.asarray(predictions)
     q = np.full(k := p.shape[0], epsilon/k)
 
